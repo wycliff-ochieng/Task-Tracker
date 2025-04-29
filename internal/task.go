338ebe983c5b1e6de27ef81a132fcac681cb8de1
wycliff <wyckieochieng91@gmail.com>
2025-04-29 10:44:11 +0300

    first

---
+++ internal/task.go
@@ -0,0 +1,23 @@
+package internal
+
+import (
+	"time"
+)
+
+type status string
+
+const (
+	STATUS_TODO        status = "todo"
+	STATUS_IN_PROGRESS status = "in-progree"
+	STATUS_DONE        status = "done"
+)
+
+type Task struct {
+	ID          int32     `json:"id"`
+	Description string    `json:"description"`
+	Status      status    `json:"status"`
+	CreatedAt   time.Time `json:"createdat"`
+	UpdatedAt   time.Time `json:"updatedat"`
+}
+
+type NewTask struct{}
